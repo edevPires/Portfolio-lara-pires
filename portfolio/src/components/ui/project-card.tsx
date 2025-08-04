24b95964be520de0170cbd4c5f6a6c93408e8af2
edevPires <133178329+edevPires@users.noreply.github.com>
2025-08-03 22:28:32 -0300

    Minor responsive fixes

--- portfolio/src/components/ui/project-card.tsx
+++ portfolio/src/components/ui/project-card.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import clsx from "clsx";
 
 interface Project {
@@ -13,10 +13,15 @@ interface ProjectCardProps {
 }
 
 const ProjectCard: React.FC<ProjectCardProps> = ({ className, project }) => {
-  console.log(project);
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleToggleClick = () => {
+    setIsClicked(!isClicked);
+  };
 
   return (
     <div
+      onClick={handleToggleClick}
       className={clsx(
         `group relative flex flex-col justify-end grow h-[25rem] rounded-2xl overflow-hidden`,
         `hover:cursor-pointer`,
@@ -26,15 +31,19 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ className, project }) => {
       <div
         className={clsx(
           `absolute inset-0 bg-cover bg-center`,
-          `transition-transform duration-500 ease-in-out group-hover:scale-110`,
+          `transition-transform duration-500 ease-in-out`,
+          `group-hover:scale-110`,
+          isClicked && `scale-110`, // Aplica o zoom quando clicado
         )}
         style={{ backgroundImage: `url('${project.path}')` }}
       />
       <div
         className={clsx(
-          `relative z-10 opacity-0 h-3/4 flex flex-col justify-end p-5 z-[1]`,
-          `transition-opacity duration-300 group-hover:opacity-100`,
+          `relative opacity-0 h-3/4 flex flex-col justify-end p-5 z-[1]`,
+          `transition-opacity duration-300`,
+          `group-hover:opacity-100`,
           `bg-linear-to-t from-black from-5% to-transparent`,
+          isClicked && `opacity-100`, // Aplica a opacidade quando clicado
         )}
       >
         <h1 className="text-2xl text-start text-primary font-mont-bold w-full">
